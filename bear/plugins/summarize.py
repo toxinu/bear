855e4a9c08babd96f140e9864eb248dbe9cfd4fe
toxinu <toxinu@gmail.com>
2014-03-27 14:25:55 +0100

    Added summarize skel

---
+++ bear/plugins/summarize.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Description
+===========
+This plugin provide a summarize of a entry description.
+
+Install
+=======
+Nothing.
+
+Example
+=======
+Nothing.
+"""
+import logging
+from .base import BasePlugin
+
+logger = logging.getLogger('plugins.summarize')
+
+
+class SummarizePlugin(BasePlugin):
+    pass
